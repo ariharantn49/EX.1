dde2e5cd8321b55cab145d5077645b3b5e9fe499
ariharantn49 <ariharan9655@gmail.com>
2026-02-16 14:22:16 +0530

    Add string manipulation examples in PROGRAM.py

--- PROGRAM.py
+++ PROGRAM.py
@@ -1,3 +1,11 @@
-name="python"
-print(name)
-a=10
+S1="python"
+S2="programming"
+print("string 1is :",S1)
+print("string 2is :",S2)
+print("concatenation:",S1+S2)
+print("concatenation:",S1+""+S2)
+print("length of string 1is:",len(S1))
+print("length of string 2is:",len(S2))
+print("first character of string 1:",S1[0])
+print("last character of string 1:",S1[-1])
+print("substring of S1:",S1[0:3])
